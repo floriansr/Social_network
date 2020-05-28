0ed84bf9756796a3d8320e44d0fea5d34c6425f3
floriansr <sueurfloriansf@gmail.com>
2020-05-28 22:43:51 +0200

    OtherUser Profil page done

--- src/pages/OtherProfile/index.jsx
+++ src/pages/OtherProfile/index.jsx
@@ -1,93 +1,67 @@
-import React, {useEffect} from "react";
-import { useParams } from "react-router-dom";
-
+import React, { useEffect } from "react";
 import Loader from "react-loader";
-
 import { useSelector, useDispatch } from "react-redux"
+import { useParams } from "react-router-dom";
 import { Descriptions, Card } from 'antd';
 import shortid from "shortid";
 import Cookies from 'js-cookie'
-
 import { setDetailsUser, setPostsUser } from "../../redux";
 
-
-
 const OtherProfile = () => {
+
   const dispatch = useDispatch();
   const { userSlug } = useParams();
   const token = Cookies.get('token')
   const allPosts = useSelector(state => state.posts.posts);
+
+useEffect(() => {
+     
+    const checkProfileId = allPosts.find((x) => {
+       if (x.user && x.user.username === userSlug) {
+         return true
+       } else {
+         return false
+       }
+     }) || []
+      console.log(checkProfileId)
+    
+    if (checkProfileId.user) {
+         fetch(`https://api-minireseausocial.mathis-dyk.fr/users/${checkProfileId.user.id}`, {
+           method: 'get',
+           headers: {
+             'Authorization': `Bearer ${token}`,
+             'Content-Type': 'application/json'
+           },
+         })
+           .then(response => response.json())
+           .then(response => {
+             dispatch(setDetailsUser(response))
+           })
+           .catch(error => console.log(error));
+         fetch(`https://api-minireseausocial.mathis-dyk.fr/posts?user.id=${checkProfileId.user.id}`, {
+           method: 'get',
+           headers: {
+             'Authorization': `Bearer ${token}`,
+             'Content-Type': 'application/json'
+           },
+         })
+           .then(response => response.json())
+           .then(response => {
+             dispatch(setPostsUser(response))
+           })
+           .catch(error => console.log(error));
+          }
+       }, [allPosts, userSlug, token, dispatch]);
+
   const thisUser = useSelector(state => state.other_user_data);
 
-
-
-  // GET USERPAGE DETAILS WITH TOKEN
-
-     useEffect(() => {
-     const checkProfileId = allPosts.find((x) => x.user.username === userSlug)
-
-      console.log(checkProfileId)
-
-       fetch(`https://api-minireseausocial.mathis-dyk.fr/users/${checkProfileId.id}`, {
-         method: 'get',
-         headers: {
-           'Authorization': `Bearer ${token}`, 
-           'Content-Type': 'application/json'
-         },
-       })
-         .then(response => response.json())
-         .then(response => {
-           dispatch(setDetailsUser(response))
-         })
-         .catch(error => console.log(error));
-
-
-      fetch(`https://api-minireseausocial.mathis-dyk.fr/posts?user.id=${checkProfileId.id}`, {
-         method: 'get',
-         headers: {
-           'Authorization': `Bearer ${token}`, 
-           'Content-Type': 'application/json'
-         },
-       })
-         .then(response => response.json())
-         .then(response => {
-           dispatch(setPostsUser(response))
-         })
-         .catch(error => console.log(error));
-
-      }, [allPosts, userSlug, token, dispatch]);
-
-  // // GET USERPAGE POSTS WITH TOKEN
-
-  //    useEffect(() => {
-  //      const checkProfileId = allPosts.find((x) => x.user.username === userSlug)
-
-  //      fetch(`https://api-minireseausocial.mathis-dyk.fr/posts?user.id=${checkProfileId.id}`, {
-  //        method: 'get',
-  //        headers: {
-  //          'Authorization': `Bearer ${token}`, 
-  //          'Content-Type': 'application/json'
-  //        },
-  //      })
-  //        .then(response => response.json())
-  //        .then(response => {
-  //          dispatch(setPostsUser(response))
-  //        })
-  //        .catch(error => console.log(error)); 
-  //  }, [allPosts,dispatch, token, userSlug]);
-
-
-
-
-  if (!thisUser) {
+  if (!thisUser || !thisUser.posts) {
     return (
       <Loader />
     )
   }
-
-	return (
-		<>
-
+  return (
+    <>
     <Descriptions
         title={`User Profile n°${thisUser.id}`}
         bordered
@@ -97,28 +71,14 @@ const OtherProfile = () => {
         <Descriptions.Item label="Description">{thisUser.description}</Descriptions.Item>
     </Descriptions>
 
-
-
-
-		</>
-	);
+    { thisUser.posts.map((x) => (
+        <div className="site-card-border-less-wrapper" key={shortid.generate()}>
+          <Card title="titre" bordered={false} style={{ width: 300 }}>
+             <p>{x.text}</p>
+          </Card>
+        </div>
+    ))}
+    </>
+  );
 };
-
-    // { thisUser.posts.map((x) => (
-    //     <div className="site-card-border-less-wrapper" key={shortid.generate()}>
-    //       <Card title="titre" bordered={false} style={{ width: 300 }}>
-    //          <p>{x.text}</p>
-    //       </Card>
-    //     </div>
-    //     ))}
-
 export default OtherProfile;
-
-
-    // { thisUser.posts.map((x) => (
-    //     <div className="site-card-border-less-wrapper" key={shortid.generate()}>
-    //       <Card title="titre" bordered={false} style={{ width: 300 }}>
-    //          <p>{x.text}</p>
-    //       </Card>
-    //     </div>
-    //     ))}
